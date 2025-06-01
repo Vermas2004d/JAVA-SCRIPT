3b30546e5c9f030b0834c68bc4146ce10868039d
Vermas2004d <142916614+Vermas2004d@users.noreply.github.com>
2025-06-01 09:45:08 +0000

    commited successfully

--- Basics/03_conversionOperation.js
+++ Basics/03_conversionOperation.js
@@ -18,19 +18,63 @@ let score = true;
 // "333abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = "";
+// let isLoggedIn = "";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-// 1 => true ; o => false
-//"" => false
-// "hitesh" => true
+// // 1 => true ; o => false
+// //"" => false
+// // "hitesh" => true
 
-let someNumber = 45;
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// let someNumber = 45;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+// ********* Operations *****************
+let value =3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(3/2);
+// console.log(2*3);
+// console.log(2%3);
+
+// let str1 = "hello";
+// let str2 = " madhav";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + "2"); //12
+// console.log("1" + 2 +2); //122
+// console.log(1 + 2 + "2"); //32
+
+// check 7.1.1 To Primitive
+console.log(3 + 4 * 5 % 3);// do not write this type of code in projects use paranthesis instead//
+
+console.log(true); //true
+console.log(+true); //1
+console.log(+"");
+
+let num1 , num2 , num3;
+
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+// learn about the prefix and postfix operator(tc39/mdn)
+
+
+
 
 

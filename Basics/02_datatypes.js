acc1694339cbd6d5f58556c36f81bea600c5f7c2
Vermas2004d <142916614+Vermas2004d@users.noreply.github.com>
2025-06-01 07:49:38 +0000

    commiting changes

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as newer version
+//-->used to write new standars like classes and arrow function//
+//ECMA STANDARDS
+
+// alert(3+3); //we are using the nodejs not browser
+
+// console.log(3+3); console.log("madhav") --> we need to maintain good readability
+
+
+//use the ecma standards for documentation(tc39 and mdn)//
+//but the ecma is the orginal standards//
+
+
+let name  = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigint
+//string => "" or ''//
+//boolean = true or false
+//null => standalone value (empty or not available ) / it is a object / type is object
+//undefined =>  type is undefined
+//symbol => used more in components(react) used for unique
+
+
+// object
+
+
+console.log(typeof(name));
+console.log(typeof undefined);
+//Note: see the specification on the ecma standards

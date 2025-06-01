2c2518ca48c36856fbc09d7e7d36d148ff617e03
Vermas2004d <142916614+Vermas2004d@users.noreply.github.com>
2025-06-01 08:14:20 +0000

    commiting file

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// conversion operation
+
+// let score = "33abc";
+// let score= null;
+// let score = undefined; -->gives NaN
+let score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+// NaN --> not a number (that's why we use the type script)
+
+
+// "33" => 33
+// "333abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; o => false
+//"" => false
+// "hitesh" => true
+
+let someNumber = 45;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+

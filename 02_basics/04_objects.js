1be0f352c8ceffce160227a2fa2fe403d5f86f7b
Vermas2004d <142916614+Vermas2004d@users.noreply.github.com>
2025-06-04 08:06:29 +0000

    commit

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -51,19 +51,65 @@ const users = [
 ]
 
 // users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log( Object.keys(tinderUser));
-//this makes the keys of the tinderUser object to array
-// that we will iterate through the array and make the keys  of the object in our use.
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));//it gives the key value paired array
+// console.log( Object.keys(tinderUser));
+// //this makes the keys of the tinderUser object to array
+// // that we will iterate through the array and make the keys  of the object in our use.
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));//it gives the key value paired array
 
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 //to check the property before using it//
 
- 
+
+
+//destructuring of the object --> starts
+
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor)
+
+
+
+
+//Examples of the react props where we used the object destructuring
+// const navbar = (props.company) => {
+
+// }
+
+// const navbar = ({company}) =>{
+
+// }
+
+// navbar(company = "hitesh")
+
+
+// `{
+//     "name" : "hitesh"
+//     "coursename": "js in hindi"
+//     "price" : "free"
+// }`
+
+//when we call the api then it gives the content in the form of json
+//when we handle api's and then some time we get difficult infomation that is difficult to understand 
+//and that is in the object or json format and 
+//and to understand that we used the json formatter to fomat the data 
+// , that we can understand the data
+
+
+
+
+
+
 
 
 
